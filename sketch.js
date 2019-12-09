c3d756e43fc4c1b4987f69922fc157f56005abab
rady1267 <57379401+rady1267@users.noreply.github.com>
2019-12-10 01:36:12 +0900

    기말용

--- sketch.js
+++ sketch.js
@@ -1,14 +1,68 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
+
+let r1, r2, r3, r4;
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  createCanvas(710, 400);
+  fill(255, 204);
+  noStroke();
+  r1 = new MRect(1, 134.0, 0.532, 0.1 * height, 10.0, 1);
+  r2 = new MRect(20, 44.0, 0.166, 0.4 * height, 5.0, 1);
+  r3 = new MRect(20, 58.0, 0.332, 0.2 * height, 10.0, 1);
+  r4 = new MRect(10, 120.0, 0.0498, 0.2 * height, 10.0, 1);
+
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY, 100, 100);
+  background(0);
+
+  r1.display();
+  r2.display();
+  r3.display();
+  r4.display();
+
+  r1.move(mouseX - width / 2, mouseY + height * 0.1, 30);
+  r2.move((mouseX + width * 0.05) % width, mouseY + height * 0.025, 20);
+  r3.move(mouseX / 4, mouseY - height * 0.025, 40);
+  r4.move(mouseX - width / 2, height - mouseY, 50);
+
+
+
+}
+
+
+
+class MRect {
+  constructor(iw, ixp, ih, iyp, id, it) {
+    this.w = iw; // single bar width
+    this.xpos = ixp; // rect xposition
+    this.h = ih; // rect height
+    this.ypos = iyp; // rect yposition
+    this.d = id; // single bar distance
+    this.t = it; // number of bars
+  }
+
+
+
+
+  move(posX, posY, damping) {
+    let dif = this.ypos - posY;
+    if (abs(dif) > 1) {
+      this.ypos -= dif / damping;
+    }
+    dif = this.xpos - posX;
+    if (abs(dif) > 1) {
+      this.xpos -= dif / damping;
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.t; i++) {
+      rect(
+        this.xpos + i * (this.d + this.w),
+        this.ypos,
+        this.w,
+        height * this.h
+      );
+    }
+  }
 }
